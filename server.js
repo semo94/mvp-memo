f09477a9ded5dccc132f4ab299882482fe9538b8
semo94 <saleem.bakri94@gmail.com>
2017-02-04 23:32:08 +0200

    edit server port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./memoServer.js');
 var express = require('express');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
